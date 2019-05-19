7e9551bb3f1f0053744228174d3ad89ea5dbf46c
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-19 18:24:48 -0400

    App was function but now it's a class

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import "./App.css";
 import Subject from "./Subject";
 import Content from "./Content";
+import NavBar from "./navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <Subject />
-      <Content />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject />
+        <NavBar />
+        <Content />
+      </div>
+    );
+  }
 }
 
 export default App;
