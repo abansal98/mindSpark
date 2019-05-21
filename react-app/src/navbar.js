3008ad661703908153868c1c1139c1cb3598fbb7
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-20 23:19:00 -0400

    working on NavBar...

--- react-app/src/navbar.js
+++ react-app/src/navbar.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import logo from "./logo.png";
+import { Navbar, Nav, Button, Form, FormControl } from "react-bootstrap";
 
 class NavBar extends Component {
   render() {
     return (
       <nav>
-        <span>
-          [NAVBAR AREA] <img src={logo} className="App-logo" alt="logo" />{" "}
-        </span>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">mindSpark</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#sample1">Sample1</Nav.Link>
+            <Nav.Link href="#Sample2">Sample2</Nav.Link>
+            <Nav.Link href="#Sample3">Sample3</Nav.Link>
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
       </nav>
     );
   }
