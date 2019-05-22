27d53b5d0c2ef13cace34a9e79fdaae134387fd5
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-21 22:22:15 -0400

    put some dummy text with loop using new logic in React!

--- react-app/src/Content.js
+++ react-app/src/Content.js
@@ -1,79 +1,39 @@
 import React, { Component } from "react";
 
 class Content extends Component {
+  // dummyDataLoop = () => {
+  //   var rows = [];
+  //   for (let i = 0; i < 50; i++) {
+  //     rows.push(<div>dummy data</div>);
+  //   }
+  //   return rows;
+  // };
+  dummyDataLoop = () => [...Array(50)].map(x => <div>dummy data</div>);
+
   render() {
     return (
       <article>
-        <div>[CONTENT AREA]</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-        <div>dummy data</div>
-
-        <div>[CONTENT AREA]</div>
+        <div>[CONTENT AREA START]</div>
+        {this.dummyDataLoop()}
+        <div>[CONTENT AREA END]</div>
       </article>
     );
   }
 }
 
 export default Content;
+
+// class A extends React.Component {
+//   functionA () => {
+//     return (
+//       아래의 render 안 div 안에 보여줄 뷰를 리턴
+//     )
+//   }
+//   render() {
+//     return (
+//     <div>
+//       {this.functionA()}
+//     </div>
+//     )
+//   }
+// }
