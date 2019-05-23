7e001a1d1bc7f37f569b26c061afdbdf6ebad201
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-23 17:20:32 -0400

    page appending problem solved

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 import Content from "./Content";
-import NavBar from "./navbar";
+import NavBar from "./Navbar";
 import Footer from "./Footer";
 import SignUp from "./SignUp";
+import Home from "./Home";
 import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <switch>
-        <Route exact path="" render={() => <Content />} />
-        <Route exact path="/" render={() => <Content />} />
+        {/* <Route exact path="" render={() => <Home />} /> */}
+        <Route exact path="/" render={() => <Home />} />
         <Route exact path="/signup" render={() => <SignUp />} />
       </switch>
     );
