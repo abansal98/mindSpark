4d333c7b0c4d108c1a329194e350c921ff9d6caf
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-23 17:22:23 -0400

    .

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import SignUp from "./SignUp";
 
 ReactDOM.render(
   <BrowserRouter>
