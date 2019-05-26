9be7ae0281b67d92e195a114c6f5c7c71aaf09f8
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-25 22:15:09 -0400

    adding meaningful dummy data and logic

--- react-app/src/Content.js
+++ react-app/src/Content.js
@@ -9,12 +9,12 @@ const quotes = [
   "I could either watch it happen or be a part of it"
 ];
 const authors = ["Thomas A. Edison", "Peter Ducker", "Steve Jobs", "Elon Musk"];
-const ratings = ["1/5", "2/5", "3/5", "4/5", "5/5"];
+const ratings = ["★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★"];
 
 class Content extends Component {
   // dummyDataLoop = () => {
   //   var rows = [];
-  //   for (let i = 0; i < 50; i++) {
+  //   for (let i = 0; i < 50;  i++) {
   //     rows.push(<div>dummy data</div>);
   //   }
   //   return rows;
@@ -25,11 +25,11 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <QuoteBox quote={quotes[0]} author={authors[0]} rating={ratings[3]} />
-        <QuoteBox quote={quotes[1]} author={authors[1]} rating={ratings[4]} />
-        <QuoteBox quote={quotes[2]} author={authors[2]} rating={ratings[4]} />
-        <QuoteBox quote={quotes[3]} author={authors[2]} rating={ratings[2]} />
-        <QuoteBox quote={quotes[4]} author={authors[3]} rating={ratings[4]} />
+        <QuoteBox quote={quotes[0]} author={authors[0]} rating={ratings[2]} />
+        <QuoteBox quote={quotes[1]} author={authors[1]} rating={ratings[3]} />
+        <QuoteBox quote={quotes[2]} author={authors[2]} rating={ratings[1]} />
+        <QuoteBox quote={quotes[3]} author={authors[2]} rating={ratings[4]} />
+        <QuoteBox quote={quotes[4]} author={authors[3]} rating={ratings[0]} />
       </article>
     );
   }
