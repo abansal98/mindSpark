a4868a10f3ae4be72f8f7b06b900405f5f6938c2
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-30 15:16:56 -0400

    new component 'About'

---
+++ react-app/src/components/About/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./About.css";
+import NavBarSignin from "../Navbar/NavbarSignin";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="aboutBody">
+        <NavBarSignin />
+      </div>
+    );
+  }
+}
+
+export default About;
