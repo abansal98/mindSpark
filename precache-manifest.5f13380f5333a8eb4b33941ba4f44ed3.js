996113361e258dccf80c927ed2843de8058591b6
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-06-02 17:51:09 -0400

    Updates

---
+++ precache-manifest.5f13380f5333a8eb4b33941ba4f44ed3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81dbf34a8db64a0525f9b6869e62c292",
+    "url": "/mindSpark_App/index.html"
+  },
+  {
+    "revision": "8423711a8c78d374411c",
+    "url": "/mindSpark_App/static/css/main.16f508fc.chunk.css"
+  },
+  {
+    "revision": "428f57193ed0549dd82e",
+    "url": "/mindSpark_App/static/js/2.9e97bed7.chunk.js"
+  },
+  {
+    "revision": "8423711a8c78d374411c",
+    "url": "/mindSpark_App/static/js/main.1d544553.chunk.js"
+  },
+  {
+    "revision": "94b15caea508f8880028",
+    "url": "/mindSpark_App/static/js/runtime~main.52c8c948.js"
+  },
+  {
+    "revision": "81c0462f37aa61717fba119c31b79521",
+    "url": "/mindSpark_App/static/media/404.81c0462f.jpg"
+  },
+  {
+    "revision": "f322cb5a8241ef68dfdcaf7811c565da",
+    "url": "/mindSpark_App/static/media/baseline-account_circle-24px.f322cb5a.svg"
+  },
+  {
+    "revision": "45b30f6af163a69443f70b9e363a9a6e",
+    "url": "/mindSpark_App/static/media/baseline-create-24px.45b30f6a.svg"
+  },
+  {
+    "revision": "6300210062b200624d0e4d0fffaa6bd3",
+    "url": "/mindSpark_App/static/media/baseline-exit_to_app-24px.63002100.svg"
+  },
+  {
+    "revision": "46d99f250decba8955fc79c9c15eaa63",
+    "url": "/mindSpark_App/static/media/concretetree.46d99f25.jpg"
+  },
+  {
+    "revision": "2ed00915c4b2b548699b605130b4868b",
+    "url": "/mindSpark_App/static/media/logo.2ed00915.png"
+  }
+]);
