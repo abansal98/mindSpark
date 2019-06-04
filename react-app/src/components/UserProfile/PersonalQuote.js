207ea22758573949d2c58f37a2893968910c4470
papanchani1 <ppanchani98@gmail.com>
2019-06-04 17:57:59 -0400

    UserProfiel v1

---
+++ react-app/src/components/UserProfile/PersonalQuote.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PersonalQuote extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Personal Quote Listing</h1>
+  </React.Fragment >
+      );
+    }
+}
+
+export default PersonalQuote;
---
+++ react-app/src/components/UserProfile/ReminderForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ReminderForm extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Reminder Set Form</h1>
+  </React.Fragment >
+      );
+    }
+}
+
+export default ReminderForm;
