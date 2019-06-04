f0f2f61c6ef048993142ca667eccdeea31191c95
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-06-04 11:19:45 -0400

    improving 'aboutteam' component

--- react-app/src/components/About/Aboutteam.js
+++ react-app/src/components/About/Aboutteam.js
@@ -2,35 +2,44 @@ import React, { Component } from "react";
 import logo from "../../icons/logo.png";
 
 import "./Aboutteam.css";
+import { Container, Row, Jumbotron } from "react-bootstrap";
 
 class Aboutteam extends Component {
   render() {
     return (
-      <div className="aboutteamBody">
-        <div className="container">
-          <div className="text-center ">
-            <img
-              className="d-block mx-auto mb-4"
-              src={logo}
-              alt=""
-              width="100"
-              height="100"
-            />
-            <h1>"mindSpark"</h1>
-            <p className="lead">
-              MindSpark is a web application that let’s users share quotes that
-              inspirit one another
-              {/* “mindSpark” is an app that encourages people through quotes and
+      <Container className="aboutteamBody">
+        <Row className="justify-content-center mt-3">
+          <img
+            className="d-block mx-auto mb-4"
+            src={logo}
+            alt=""
+            width="100"
+            height="100"
+          />
+        </Row>
+        <Row className="justify-content-center">
+          <h1>"mindSpark"</h1>
+        </Row>
+        <Row className="justify-content-center">
+          <p className="lead text-center">
+            MindSpark is a web application that let’s users share quotes that
+            inspirit one another
+            {/* “mindSpark” is an app that encourages people through quotes and
               messages. It helps people who are having tough time with emotional
               problem. It also lets you share your own quotes to help community
               build curated ideas. */}
-            </p>
-            <p>
-              It helps people who are having tough time with emotional problem.
-              It also lets you upload your own quotes which can be shared to
-              help other people.
-            </p>
-            <h3 className="text-uppercase">Inspirit yourself</h3>
+          </p>
+          <p className="lead text-center">
+            It helps people who are having tough time with emotional problem. It
+            also lets you upload your own quotes which can be shared to help
+            other people.
+          </p>
+        </Row>
+        <Row className="text-center">
+          <h3 className="text-uppercase">Inspirit yourself</h3>
+        </Row>
+        <div className="container">
+          <div className="text-center ">
             <p>
               Classified quotes will help you to be motivated based on your
               mind-state and mood
@@ -47,7 +56,7 @@ class Aboutteam extends Component {
             </p>
           </div>
         </div>
-      </div>
+      </Container>
     );
   }
 }
