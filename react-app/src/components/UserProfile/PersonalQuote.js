6e22d9a3b5b2e5f0b6d151d1c1eb2789e12a65ff
Young Min Ko <ymko@myseneca.ca>
2019-06-04 18:24:36 -0400

    update uploadQuote

--- react-app/src/components/UserProfile/PersonalQuote.js
+++ react-app/src/components/UserProfile/PersonalQuote.js
@@ -1,12 +1,62 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import $ from "jquery";
+import Button from "react-bootstrap/Button";
 
-class PersonalQuote extends Component {
+class PersonalQuote extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            quote: "",
+        }
+
+
+    };
+
+    handleInput(e)
+    {
+        this.setState({quote: e.target.value});
+    }
+
+    addQuote(e)
+    {
+        e.preventDefault();
+        console.log(this.state.quote);
+        $.ajax({
+            url: "/db/quote",
+            method: "POST",
+            data: {
+                text: this.state.quote
+      }
+    })
+      .then(msg => {
+        alert(msg);
+    
+      })
+      .fail(err => {
+        alert(err.responseText);
+      });
+      
+    }
+
     render() {
         return (
-            <React.Fragment>
-                <h1>Personal Quote Listing</h1>
-  </React.Fragment >
-      );
+            <div className="upload">
+            <form onSubmit={this.addQuote.bind(this)}>
+
+                <textarea onChange={this.handleInput.bind(this)}
+                name="quote"
+                placeholder = "Enter Quote"
+                value = {this.state.quote}/>
+       
+            <button 
+                className="submit"
+                type="submit">Submit
+            </button>   
+            </form> 
+            </div>
+         );
     }
 }
 
