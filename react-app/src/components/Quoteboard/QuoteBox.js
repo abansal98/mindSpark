45061d0a5107e8ef5500962ada29707314ed9ce3
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-06-04 23:32:38 -0400

    modifying syntax for better React+ReactBoostrap

--- react-app/src/components/Quoteboard/QuoteBox.js
+++ react-app/src/components/Quoteboard/QuoteBox.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
 import "./QuoteBox.css";
+import { Container, Row } from "react-bootstrap";
 
 class QuoteBox extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Container className="quoteboxContainer">
         <div className="quoteBoxBg">
           <div className="quoteBox">
-            <h3 className="quoteBoxQuoteH3">{this.props.quote}</h3>
-            <div className="quoteBoxAuthorStar">
+            <Row>
+              <h3 className="quoteBoxQuoteH3">{this.props.quote}</h3>
+            </Row>
+            <Row className="quoteBoxAuthorStar justify-content-end">
               <span>{this.props.author}</span>
-              <span> </span>
-              {this.props.rating}
-            </div>
+              <span>{this.props.rating} </span>
+            </Row>
           </div>
         </div>
-      </React.Fragment>
+      </Container>
     );
   }
 }
