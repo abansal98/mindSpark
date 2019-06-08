b413d39652831853f06ccce053de9c5e5965d2be
papanchani1 <ppanchani98@gmail.com>
2019-06-08 15:47:06 -0400

    reminder form v1

--- react-app/src/components/UserProfile/ReminderForm.js
+++ react-app/src/components/UserProfile/ReminderForm.js
@@ -4,9 +4,46 @@ class ReminderForm extends Component {
     render() {
         return (
             <React.Fragment>
-                <h1>Reminder Set Form</h1>
-  </React.Fragment >
-      );
+                <h2>Set Quote Reminder</h2>
+                <div className="wrapper" ref={this.props.containerRef}>
+                    <div className="form-wrapper">
+                        <div className="header">
+                        </div>
+                        <form>
+                        <div class="form-group">
+                            <label for="sel1">Select Category:</label>
+                            <select multiple class="form-control" id="sel1">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                            </select>
+                            </div>
+                            <div class="form-group">
+                            <label for="sel1">Select Days:</label>
+                            <select multiple class="form-control" id="sel1">
+                                <option>Monday</option>
+                                <option>Tuesday</option>
+                                <option>Wednesday</option>
+                                <option>Thursday</option>
+                                <option>Friday</option>
+                                <option>Saturday</option>
+                                <option>Sunday</option>
+                            </select>
+                            </div>
+                            <div className="form-group">
+                                <div className="email">
+                                    <label htmlFor="email">Time</label>
+                                    <input type="time" id="appt" name="appt" required/>
+                                </div>
+                            </div>
+                            <button className="btn btn-primary" type="submit">
+                                Set Reminder
+                                </button>
+                        </form>
+                    </div>
+                </div>
+            </React.Fragment >
+        );
     }
 }
 
