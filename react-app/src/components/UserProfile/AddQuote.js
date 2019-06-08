b21519cd955833897a5b8a36a70901f7e67fa107
papanchani1 <ppanchani98@gmail.com>
2019-06-08 17:36:13 -0400

    Fixed Quote Add in UserProfile

--- react-app/src/components/UserProfile/AddQuote.js
+++ react-app/src/components/UserProfile/AddQuote.js
@@ -1,58 +1,105 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import $ from "jquery";
 import Button from "react-bootstrap/Button";
 
-class addQuote extends Component {
-    constructor(props) {
+const quoteRegex = RegExp(/^[a-z]{0,10}$/);
+
+class AddQuote extends Component
+{
+    constructor(props)
+    {
         super(props);
         this.state = {
             quote: "",
+            quoteValid: false,
+            error: {
+                quote: ""
+            }
         }
-
-
     };
 
-    handleInput(e) {
-        this.setState({ quote: e.target.value });
-    }
-
-    addQuote(e) {
+    handleSubmit(e)
+    {
         e.preventDefault();
-        console.log(this.state.quote);
         $.ajax({
-            url: "db/quote",
+            url: "/db/quote",
             method: "POST",
             data: {
-                quoteContent: this.state.quote,
-            }
-        })
-            .then(msg => {
-                alert(msg);
-                this.setState({ quote: "" });
-            })
-            .fail(err => {
-                alert(err.responseText);
-            });
+                quote: this.state.quote
+      }
+    })
+      .then(msg => {
+        alert(msg);
+    
+      })
+      .fail(err => {
+        alert(err.responseText);
+      });  
     }
+
+    handleUserInput(e)
+    {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({quote: e.target.value}, () => {
+            this.validateField(name, value);
+        });
+    }
+
+validateField(fieldName, value) {
+    let errors = this.state.error;
+    let quoteValid = this.state.quoteValid;
+
+    switch (fieldName) {
+      case "quote":
+        quoteValid = quoteRegex.test(value);
+        errors.quote = quoteValid ? "" : " has a limit of 10 characters.";
+        break;
+      default:
+        break;
+    }
+    this.setState(
+        {
+          error: errors,
+          quoteValid: quoteValid,
+        },
+        this.validateForm
+      );
+}
+
+validateForm() {
+    this.setState({
+      formValid:
+        this.state.quoteValid
+    });
+  }
 
     render() {
         return (
             <div className="upload">
-                <form onSubmit={this.addQuote.bind(this)}>
+            <form onSubmit={this.handleSubmit.bind(this)}>
 
-                    <textarea onChange={this.handleInput.bind(this)}
-                        name="quote"
-                        placeholder="Enter Quote"
-                        value={this.state.quote} />
-
-                    <button
-                        className="submit"
-                        type="submit">Submit
-            </button>
-                </form>
+                <textarea
+                className={`form-control ${
+                    this.state.error.quote ? "invalid" : ""
+                  }`}
+                onChange={this.handleUserInput.bind(this)}
+                name="quote"
+                placeholder = "Enter Quote"
+                value = {this.state.quote}
+                />
+                <div className="invalid-name">
+                    {this.state.error.quote}
+                  </div>
+       
+            <button 
+                className="submit"
+                type="submit">Submit
+            </button>   
+            </form> 
             </div>
-        );
+         );
     }
 }
 
-export default addQuote;
+export default AddQuote;
