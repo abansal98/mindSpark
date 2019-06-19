1b3c852ae4df7dc10b9fd7a25afab04891281eed
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-06-18 20:25:57 -0400

    deleted CatID, changed categoryName type

--- react-app/database/category.js
+++ react-app/database/category.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var category = new Schema({
-  categoryID: String,
-  category: [],
+  categoryName: String,
   description: String
 });
 
@@ -12,8 +11,8 @@ var categoryModel = mongoose.model("category", category);
 module.exports = {
   categoryModel,
 
-  addCategory: function (data) {
-    return new Promise(function (resolve, reject) {
+  addCategory: function(data) {
+    return new Promise(function(resolve, reject) {
       var category_data = new categoryModel({
         categoryID: data.categoryID,
         categoryName: data.categoryName,
@@ -33,10 +32,10 @@ module.exports = {
     });
   },
 
-  removeCategory: function (categoryID) { },
+  removeCategory: function(categoryID) {},
 
-  fetchCategoryList: function () {
-    return new Promise(function (resolve, reject) {
+  fetchCategoryList: function() {
+    return new Promise(function(resolve, reject) {
       categoryModel
         .find({})
         .exec()
