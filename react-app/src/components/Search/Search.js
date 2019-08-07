acd86a3f960f5f84ef967e95ae6b2b6b7d65bf21
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-08-06 23:09:33 -0400

    audo-format by VSCODE

--- react-app/src/components/Search/Search.js
+++ react-app/src/components/Search/Search.js
@@ -1,63 +1,68 @@
-import React, {Component} from 'react';
-import {FormGroup} from 'react-bootstrap';
-import $ from 'jquery';
-import Suggestions from './Suggestions';
+import React, { Component } from "react";
+import { FormGroup } from "react-bootstrap";
+import $ from "jquery";
+import Suggestions from "./Suggestions";
 
 class Search extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            searchInfo: [],
-            search: ""
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInfo: [],
+      search: ""
+    };
+  }
 
-    }
+  handleInputChange = e => {
+    e.preventDefault();
+    this.setState(
+      {
+        search: e.target.value
+      },
+      () => {
+        if (this.state.search) {
+          this.Search();
+        }
+      }
+    );
+  };
 
-    handleInputChange = (e) => {
-        e.preventDefault();
-        this.setState({
-            search: e.target.value
-        }, () => {
-            if (this.state.search) {
-                this.Search();
-            }
-        })
-    }
+  Search = () => {
+    console.log("Search called");
+    $.ajax({
+      url: "db/search/suggest/" + this.state.search,
+      method: "GET"
+    }).then(data => {
+      console.log(data);
+      const abc = data.map(obj => ({
+        quote: obj.text,
+        author: obj.author,
+        category: obj.category,
+        quoteId: obj._id
+      }));
 
-    Search = () => {
-        console.log("Search called");
-        $.ajax({
-            url: "db/search/suggest/" + this.state.search,
-            method: "GET"
-        })
-        .then(data => {
-            console.log(data);
-            const abc = data.map(obj => ({
-                quote: obj.text,
-                author: obj.author,
-                category: obj.category,
-                quoteId: obj._id
-            }));
+      this.setState({
+        searchInfo: abc
+      });
+    });
+  };
 
-            this.setState({
-                searchInfo: abc
-            })
-        })
-    }
-
-
-    render() {
-        return (
-            <React.Fragment>
-                <form>
-                    <input onChange={this.handleInputChange} value={this.state.search} placeholder="Search for.."/>
-                    <Suggestions results={this.state.searchInfo} username={this.props.username}/>
-                </form>
-
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <form className="ml-3">
+          <input
+            onChange={this.handleInputChange}
+            value={this.state.search}
+            placeholder="Search for.."
+          />
+          <Suggestions
+            results={this.state.searchInfo}
+            username={this.props.username}
+          />
+        </form>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Search;
