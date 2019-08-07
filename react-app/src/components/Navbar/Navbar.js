99e31743bb28bfc4bff69dd2ebfc49468826f933
papanchani1 <ppanchani98@gmail.com>
2019-08-06 22:24:41 -0400

    Seperate button for AddQuote

--- react-app/src/components/Navbar/Navbar.js
+++ react-app/src/components/Navbar/Navbar.js
@@ -44,7 +44,8 @@ class NavBar extends Component {
           >
             <Navbar.Brand>Hello, {this.props.username}</Navbar.Brand>
             <Nav>
-              <Nav.Link href="/userprofile">User Profile</Nav.Link>
+              <Nav.Link href="/userProfileAddQuote">Add Quote</Nav.Link>
+              <Nav.Link href="/userProfileUserProfile">User Profile</Nav.Link>
               <Nav.Link href="/db/logout">Sign Out</Nav.Link>
             </Nav>
           </Navbar.Collapse>
